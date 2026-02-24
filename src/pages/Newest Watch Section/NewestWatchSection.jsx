3b16630c4aa18809b8d066e2ac52560deb172ba7
C-G <kelvindonye@gmail.com>
2026-02-24 11:20:09 +0100

    Decreased the scroll for the newestWatch section.

--- src/pages/Newest Watch Section/NewestWatchSection.jsx
+++ src/pages/Newest Watch Section/NewestWatchSection.jsx
@@ -20,7 +20,7 @@ const NewestWatchSection = () => {
   // })
 
   return (
-    <section ref={ref} className='relative h-[800vh] w-360 bg-white'>
+    <section ref={ref} className='relative h-[700vh] w-360 bg-white'>
       <div className='sticky top-0 px-50 py-15 w-full bg-black mix-blend-difference'>
         <NewestInfo scrollYProgress={scrollYProgress} />
         <AnimatedBackground scrollYProgress={scrollYProgress}/>
