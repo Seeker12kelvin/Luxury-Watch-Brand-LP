ec4ef716aa2f3973cf880c14e932edcfa56e6372
C-G <kelvindonye@gmail.com>
2026-03-18 01:40:15 +0100

    Slider change

--- src/pages/Slide Show Section/Content/Slider.tsx
+++ src/pages/Slide Show Section/Content/Slider.tsx
@@ -26,7 +26,7 @@ const Slider = ({ data }: SliderProps): JSX.Element => {
 
   return (
     <div
-      className="relative overflow-x-hidden h-100 max-sm:h-full"
+      className="relative overflow-hidden h-100 max-sm:h-full"
       style={{ width: width }}
     >
       <motion.div
