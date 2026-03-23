374c42f22792da7c165bb63be5103ca81e1a482d
C-G <kelvindonye@gmail.com>
2026-03-23 09:10:56 +0100

    Refractored the way letters are rendered in the footer logo segment of the fotter section

--- src/pages/Footer/Content/FooterLogo.tsx
+++ src/pages/Footer/Content/FooterLogo.tsx
@@ -4,6 +4,9 @@ import { IoIosArrowRoundForward } from 'react-icons/io';
 import Comp_Logo from '../../../images/Footer-Logo-Container.svg';
 
 const FooterLogo = (): JSX.Element => {
+
+  const letters: string[] = ['B', 'R', 'A', 'N', 'T', 'I', 'U', 'M']
+
   return (
     <div className='w-full h-[276.28px] max-[481px]:h-50 flex flex-col gap-10 items-center justify-end'>
     
@@ -31,61 +34,15 @@ const FooterLogo = (): JSX.Element => {
         viewport={{once: true}}
         className='flex items-center h-53.5 gap-2 w-full justify-center'>
 
-        <motion.h1
-          initial={{scale: 1}}
-          whileHover={{scale: 1.1}}
-          className='text-[20vw] font-light tracking-[-0.32px] text-[#181818]'>
-          B
-        </motion.h1>
-
-        <motion.h1
-          initial={{scale: 1}}
-          whileHover={{scale: 1.1}}
-          className='text-[20vw] font-light tracking-[-0.32px] text-[#181818]'>
-          R
-        </motion.h1>
-
-        <motion.h1
-          initial={{scale: 1}}
-          whileHover={{scale: 1.1}}
-          className='text-[20vw] font-light tracking-[-0.32px] text-[#181818]'>
-          A
-        </motion.h1>
-
-        <motion.h1
-          initial={{scale: 1}}
-          whileHover={{scale: 1.1}}
-          className='text-[20vw] font-light tracking-[-0.32px] text-[#181818]'>
-          N
-        </motion.h1>
-
-        <motion.h1
-          initial={{scale: 1}}
-          whileHover={{scale: 1.1}}
-          className='text-[20vw] font-light tracking-[-0.32px] text-[#181818]'>
-          T
-        </motion.h1>
-
-        <motion.h1
-          initial={{scale: 1}}
-          whileHover={{scale: 1.1}}
-          className='text-[20vw] font-light tracking-[-0.32px] text-[#181818]'>
-          I
-        </motion.h1>
-
-        <motion.h1
-          initial={{scale: 1}}
-          whileHover={{scale: 1.1}}
-          className='text-[20vw] font-light tracking-[-0.32px] text-[#181818]'>
-          U
-        </motion.h1>
-
-        <motion.h1
-          initial={{scale: 1}}
-          whileHover={{scale: 1.1}}
-          className='text-[20vw] font-light tracking-[-0.32px] text-[#181818]'>
-          M
-        </motion.h1>
+        {letters.map((data: string, index: number) => (
+          <motion.h1
+            key={index}
+            initial={{scale: 1}}
+            whileHover={{scale: 1.1}}
+            className='text-[20vw] font-light tracking-[-0.32px] text-[#181818]'>
+            {data}
+          </motion.h1>
+        ))}
 
       </motion.div>
 
--- src/pages/Show Case Section/Content/AnimatedBackground.tsx
+++ src/pages/Show Case Section/Content/AnimatedBackground.tsx
@@ -20,8 +20,6 @@ const AnimatedBackground = ({scrollYProgress, bg}: AnimatedBackgroundProps): JSX
       style={{translateY: y}}
       className={`absolute left-0 right-0 bottom-0 w-full bg-[${bg}] z-[-1] rounded-t-full`}
       src={fill_background}
-
-      
     />
   )
 }
