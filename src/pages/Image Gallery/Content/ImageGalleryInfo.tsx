106b1850995d0d54f6fb3d0daac473e25276bcff
C-G <kelvindonye@gmail.com>
2026-03-23 07:54:30 +0100

    sere

--- src/pages/Image Gallery/Content/ImageGalleryInfo.tsx
+++ src/pages/Image Gallery/Content/ImageGalleryInfo.tsx
@@ -4,7 +4,7 @@ import SecondSlide from './SecondSlide';
 
 const ImageGalleryInfo = (): JSX.Element => {
   return (
-    <div className='h-full w-full flex min-[1030px]:justify-between max-[1031px]:flex-col'>
+    <div className='h-full w-full flex min-[1030px]:justify-between max-[1031px]:flex-col max-[554px]:h-562.5'>
       <FirstSlide />
       <SecondSlide />
     </div>
--- src/pages/Image Gallery/ImageGallery.tsx
+++ src/pages/Image Gallery/ImageGallery.tsx
@@ -4,7 +4,7 @@ import ImageGalleryInfo from './Content/ImageGalleryInfo';
 
 const ImageGallery = (): JSX.Element => {
   return (
-    <section className='h-245 w-full max-[1031px]:h-500 max-[769px]:h-665'>
+    <section className='h-245 w-full max-[1031px]:h-500 max-[769px]:h-fit'>
       <AnimatePresence mode="wait">
         <ImageGalleryInfo />
       </AnimatePresence>
