0728fda5ca75022f6ef4d313f3fafc4dc0bb4686
C-G <kelvindonye@gmail.com>
2026-03-29 17:17:44 +0100

    Added an outro animation to the Image gallery section

--- src/pages/Image Gallery/Content/FirstSlide.tsx
+++ src/pages/Image Gallery/Content/FirstSlide.tsx
@@ -1,8 +1,10 @@
-import React, { JSX, useState } from 'react';
+import React, { JSX, useEffect, useRef, useState } from 'react';
 import type { ImageGalleryItem } from '../../../data';
 import { imageGalleryInfo } from '../../../data';
 import { AnimatePresence, motion } from 'framer-motion';
 import bg_blue_brantium from '../../../images/Second-Slider-Blue-Brantium.png';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 type Buttons = {
   buttons: string
@@ -10,6 +12,7 @@ type Buttons = {
 
 const FirstSlide = (): JSX.Element => {
   
+  const [btn, setBtn] = useState(false)
   const [bg, setBg] = useState<ImageGalleryItem>(
     {
       color: '#13386F',
@@ -28,65 +31,69 @@ const FirstSlide = (): JSX.Element => {
 
   const handleBgInfo = (color: string) => {
     const getInfo: ImageGalleryItem | undefined = imageGalleryInfo.find(data => data.color == color)
-    if (getInfo) {
+    if (getInfo){
       setBg(getInfo)
     }
   }
 
-  const textVariants: any = {
-    initial: {
-      clipPath: 'inset(0 0 100% 0)',
-      opacity: 0,
-      y: 0
-    },
-    animate: {
-      clipPath: 'inset(0 0 0% 0)',
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.6,
-        ease: 'easeInOut'
-      }
-    },
-    exit: {
-      clipPath: 'inset(100% 0 0 0)',
-      opacity: 0,
-      y: 0,
-      transition: {
-        duration: 0.6 * 0.7,
-        ease: 'easeInOut'
-      }
-    }
+  const headingRef = useRef<HTMLHeadingElement | any>(null)
+  const paragraphRef = useRef<HTMLParagraphElement | any>(null)
+  const h3Ref = useRef<HTMLHeadingElement | any>(null)
+  const imgRef = useRef<HTMLImageElement | any>(null)
+  
+  useGSAP(() => {
+    const tl = gsap.timeline()
+    const heading = headingRef.current
+    const paragraph = paragraphRef.current
+    const h3 = h3Ref.current
+
+    gsap.set(imgRef.current, { opacity: 0 })
+
+    gsap.set([heading, paragraph, h3], { clipPath: 'inset(0 0 100% 0)' })
+
+    tl
+    .to(imgRef.current, { opacity: 1, duration: 0.6 }, '<')
+
+    .to([heading, paragraph, h3], { clipPath: 'inset(0 0 0% 0)', duration: 0.6, stagger: 0.1, onComplete: () => {setBtn(false)} })
+
+  }, [bg])
+
+  const animate = (color: string) => {
+
+    const tl = gsap.timeline()
+
+   tl
+   .to(imgRef.current, { opacity: 0, duration: 0.6 })
+
+   .to([headingRef.current, paragraphRef.current, h3Ref.current], { clipPath: 'inset(0 0 100% 0)', duration: 0.2, stagger: 0.1, 
+    onComplete: () => {
+      handleBgInfo(color)
+      setBtn(true)
+    }})
   }
+
+
 
   return (
     <div
       className='h-full w-1/2 relative overflow-hidden flex max-[1031px]:w-full max-[1031px]:h-screen'>
 
-      <img src={bg?.backgroundImage} className='absolute z-10 h-281.25 aspect-167/181 object-cover object-center w-full max-w-full max-[1031px]:h-full' />
+      <img ref={imgRef} src={bg?.backgroundImage} className='absolute z-10 h-281.25 aspect-167/181 object-cover object-center w-full max-w-full max-[1031px]:h-full' />
 
       <div className='absolute z-20 h-[79%] top-1/10 max-[554px]:top-10 min-[554px]:self-stretch max-[554px]:h-[90%] left-[10%] max-[554px]:left-[5%] flex flex-col justify-between text-[#FFF]'>
 
         <div className='flex flex-col gap-2.5'>
-          <motion.h1
-            key={`${bg?.color}1-badge`}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            variants={textVariants}
+          <h1
+            ref={headingRef}
             className='text-[64px] font-semibold leading-16 tracking-[-0.04rem] max-[490px]:text-[3rem]'>
               THE <br/> {bg?.header}
-          </motion.h1>
+          </h1>
 
-          <motion.p
-            key={`${bg?.color}2-badge`}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            variants={textVariants}
+          <p
+            ref={paragraphRef}
             className='text-[1rem] max-w-150 w-112.5 font-light leading-[1.3rem] tracking-[-0.02rem] max-[490px]:text-[0.9rem] max-[490px]:max-w-full max-[490px]:w-90'>
             {bg?.description}
-          </motion.p>
+          </p>
         </div>
 
         <div className='flex flex-col gap-8'>
@@ -95,24 +102,23 @@ const FirstSlide = (): JSX.Element => {
 
             {buttons.map((data, index) => (
               <button
+                disabled={btn}
                 key={index}
-                onClick={() => handleBgInfo(data.buttons)} 
-                className={`w-6 h-6 ${bg?.color === data.buttons ? 'border-2 border-white' : ''} rounded-full`}
+                onClick={() => {
+                  animate(data.buttons)
+                }} 
+                className={`w-6 h-6 ${btn ? 'opacity-50' : ''} ${bg?.color === data.buttons ? 'border-2 border-white' : ''} rounded-full`}
                 style={{backgroundColor: data.buttons}}>
               </button>
             ))}
 
           </div>
           
-          <motion.h3
-            key={`${bg?.color}3-badge`}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            variants={textVariants}
+          <h3
+            ref={h3Ref}
             className='text-[64px] font-semibold leading-16 tracking-[-0.04rem] max-[490px]:text-[3rem]'>
               {bg?.price}
-          </motion.h3>
+          </h3>
 
         </div>
       </div>
