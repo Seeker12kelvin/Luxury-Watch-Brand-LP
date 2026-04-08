77709c975853ce9c055ebc92e0d5c8c2fe028669
C-G <kelvindonye@gmail.com>
2026-04-08 12:38:51 +0100

    Removed unnecesssary images for mobile view

--- src/pages/Newest Watch Section/NewestWatchSection.tsx
+++ src/pages/Newest Watch Section/NewestWatchSection.tsx
@@ -9,7 +9,7 @@ import useWindowSize from '../../hooks/windowSize';
 
 const NewestWatchSection = (): JSX.Element => {
 
-  const { width } = useWindowSize()
+  const { width, height } = useWindowSize()
 
   const date = new Date().getFullYear()
   const imgRef = useRef<HTMLDivElement | any>(null)
@@ -70,7 +70,7 @@ const NewestWatchSection = (): JSX.Element => {
         borderRadius: 20,
         width: 1040,
         height: 460,
-        y: 415
+        y: width <= 481 ? 280 : width <= 818 ? 300 : 415
       },
       {
         borderRadius: 0,
@@ -80,12 +80,12 @@ const NewestWatchSection = (): JSX.Element => {
         duration: 1
       }
     )
-    .to(img, {height: 336, duration: 1, ease: "none"})
-    .to(img, {height: 915, duration: 1, ease: "none"})
-    .to(img, {opacity: 0, duration: 1, ease: "none"})
-    .fromTo(div, {paddingTop: 93, paddingBottom: 93}, {paddingTop: 0, paddingBottom: 0, ease: "none"})
-    .to(background, {opacity: 1, duration: 1})
-    .to(price, {opacity: 1, duration: 1, ease: "none"})
+    .to(img, {height: 336, duration: 1})
+    .to(img, {height: width <= 481 ? 700 : width <= 818 ? 300 : 915, duration: 1})
+    .to(img, {opacity: width <= 818 ? 1 : 0, duration: width <= 818 ? 1 : 0})
+    .fromTo(div, {paddingTop: 93, paddingBottom: 93}, {paddingTop: 0, paddingBottom: 0})
+    .to(background, {opacity: width <= 818 ? 0 : 1, duration: width <= 818 ? 0 : 1})
+    .to(price, {opacity: 1, duration: 1})
 
   }, {scope: animatedDiv})
 
@@ -93,7 +93,7 @@ const NewestWatchSection = (): JSX.Element => {
   return (
     <div ref={sectRef} className='h-278.75 max-[1024px]:max-w-screen flex items-start relative flex-col justify-between bg-white w-full z-10'>
 
-      <div className={`max-[1024px]:px-26 max-[740px]:px-14 max-[573px]:px-10 max-[541px]:px-8 py-25 px-50 flex flex-col justify-between h-full w-full`}>
+      <div className={`max-[1024px]:py-20 max-[740px]:py-15 max-[573px]:py-10 max-[541px]:py-8  max-[1024px]:px-26 max-[740px]:px-14 max-[573px]:px-10 max-[541px]:px-8 py-25 px-50 flex flex-col justify-between h-full w-full`}>
 
         <div className='flex flex-col items-start max-[923px]:text-center max-[923px]:w-full max-[666px]:gap-1 max-[495px]:gap-1'>
 
@@ -103,15 +103,15 @@ const NewestWatchSection = (): JSX.Element => {
 
           </div>
 
-          <h1 ref={h1Ref} className='text-[4rem] uppercase mix-blend-difference text-white text-left max-w-260 w-full font-semibold not-italic max-[923px]:w-full max-[819px]:text-[3rem] max-[666px]:text-[2.5rem] max-[819px]:leading-12 max-[446px]:text-[2rem]'>The Brantium Grand <br/> Complication No. 1</h1>
+          <h1 ref={h1Ref} className='text-[4rem] uppercase mix-blend-difference text-white text-left max-w-260 w-full font-semibold not-italic max-[923px]:w-full max-[819px]:text-[3rem] max-[666px]:text-[2.5rem] max-[819px]:leading-12 max-[541px]:text-center max-[446px]:text-[2rem] max-[370px]:text-[1.7rem]'>The Brantium Grand <br/> Complication No. 1</h1>
 
-          <p ref={pFirstRef} className='text-[1rem] mix-blend-difference text-white max-w-150 self-stretch font-light not-italic leading-[1.3rem] tracking-[-0.02rem] max-[923px]:w-full max-[923px]:text-lg max-[923px]:leading-6 max-[819px]:text-[1rem] max-[495px]:leading-5 max-[446px]:text-[0.9rem]'>An ultra-exclusive mechanical masterpiece engineered for elite collectors and long-term investors. Hand-assembled, individually numbered, and produced in strictly limited quantities.</p>
+          <p ref={pFirstRef} className='text-[1rem] mix-blend-difference text-white max-w-150 self-stretch font-light not-italic leading-[1.3rem] tracking-[-0.02rem] max-[923px]:w-full max-[923px]:text-lg max-[923px]:leading-6 max-[819px]:text-[1rem] max-[495px]:leading-5 max-[446px]:text-[0.9rem] max-[370px]:text-[0.8rem]'>An ultra-exclusive mechanical masterpiece engineered for elite collectors and long-term investors. Hand-assembled, individually numbered, and produced in strictly limited quantities.</p>
 
         </div>
 
         <div className='flex items-center justify-between w-full max-[861px]:flex-col max-[861px]:text-center max-[861px]:gap-10'>
 
-          <p ref={pRef} className='text-[1rem] mix-blend-difference text-white max-w-150 w-full font-light not-italic leading-[1.3rem] tracking-[-0.02rem] max-[997px]:max-w-100 max-[446px]:text-[0.9rem]'>Featuring a Swiss high-complication automatic movement, precious metal casework, and exhibition finishing reserved for haute horlogerie ateliers. Designed as a generational collector’s asset.</p>
+          <p ref={pRef} className='text-[1rem] mix-blend-difference text-white max-w-150 w-full font-light not-italic leading-[1.3rem] tracking-[-0.02rem] max-[997px]:max-w-100 max-[446px]:text-[0.9rem] max-[370px]:text-[0.8rem'>Featuring a Swiss high-complication automatic movement, precious metal casework, and exhibition finishing reserved for haute horlogerie ateliers. Designed as a generational collector’s asset.</p>
 
           <div ref={priceRef} className='w-full h-full flex flex-col items-end gap-5 opacity-0 max-[861px]:items-center'>
 
@@ -124,13 +124,13 @@ const NewestWatchSection = (): JSX.Element => {
         </div>
       </div>
 
-      <div  ref={animatedDiv} className='bg-black z-[-1] absolute self-center py-6'>
+      <div ref={animatedDiv} className='bg-black z-[-1] absolute self-center py-6'>
         <div ref={innerAnimatedDiv} className='h-full relative flex items-center justify-center'>
 
           <div
             ref={animatedBackgroundRef}
-            className='absolute h-full w-full opacity-1 inset-0 bg-no-repeat'
-            style={{background: `linear-gradient(0deg, rgba(0, 0, 0, 0.40) 0%, rgba(0, 0, 0, 0.20) 100%), url(${Bg}) lightgray 50% / cover no-repeat`}}
+            className='absolute h-full w-screen opacity-0 inset-0'
+            style={{background: `linear-gradient(0deg, rgba(0, 0, 0, 0.40) 0%, rgba(0, 0, 0, 0.20) 100%), url(${Bg}) lightgray 50% / cover no-repeat`,  backgroundPosition: 'center center'}}
           />
 
           <img
